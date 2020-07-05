3a66e759648aefdcf16bc7106a1ccd35e820ba43
Hamdi Tuğ <hamditug@apple-MacBook.local>
2020-07-06 01:14:35 +0300

    first commit

---
+++ Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Tutorial.ExampleApi.Models
+{
+    public partial class Products
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int AvailableQuantity { get; set; }
+
+    }
+}
